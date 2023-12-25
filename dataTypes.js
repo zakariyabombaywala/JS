dffd4626a57e098df971e7defe75cbb6f310aee6
zakariyabombaywala <zakariyabombaywala@gmail.com>
2023-12-25 11:32:46 +0530

    js-datatypes

---
+++ dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello"); // We are using node JS, not browser
+
+console.log(3 + 3); // code redability shuold be high
+
+let name = "Zakariya";
+let age = 23;
+let isLoggedIn = true;
+let state;
+
+// number => 2 to power 53
+// BigInt
+// String=> ""
+// Boolean => true/false
+// null => standlone value / also stand for object
+// undefined =>
+// Symbol => unique
+
+//object
+
+console.log(typeof undefined);
