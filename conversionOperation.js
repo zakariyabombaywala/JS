8a4289c23b35aa13256941ca7f178a1b2412eaf0
zakariyabombaywala <zakariyabombaywala@gmail.com>
2023-12-25 15:07:03 +0530

    conversion of datatypes

---
+++ conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "Zakariya";
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumner = Number(score);
+// console.log(typeof valueInNumner);
+// console.log(valueInNumner);
+
+// "44" => 44
+// "44cdc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Zakariya" => true
+
+let someNumber = 55;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
