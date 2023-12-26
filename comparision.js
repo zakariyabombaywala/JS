04350cf9c5c0509a3b9dd7492fc524d11015ba14
zakariyabombaywala <zakariyabombaywala@gmail.com>
2023-12-26 10:36:31 +0530

    js-comparision

---
+++ comparision.js
@@ -0,0 +1,12 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("3" > 1);
+// console.log("03" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
