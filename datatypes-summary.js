b0679cf9bf58433f408b57e487a240dc7fadc0a8
zakariyabombaywala <zakariyabombaywala@gmail.com>
2023-12-26 11:35:12 +0530

    js-datatypes-summary

---
+++ datatypes-summary.js
@@ -0,0 +1,22 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefind, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoogedIn = true;
+const outsideTemp = null;
+let userEmail;
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const days = ["Mon", "Tue", "Wed"]; // Array
+
+let weekend = { 1: "Saturday", 2: "Sunday" };
+
+const myFun = function () {
+  console.log("Hello world");
+};
