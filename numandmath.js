38fa1a495eb4d4ab1ec2486931fdeff4e2a384b5
zakariyabombaywala <zakariyabombaywala@gmail.com>
2024-01-10 10:49:16 +0530

    js-number_and_math

---
+++ numandmath.js
@@ -0,0 +1,33 @@
+const score = 500;
+// console.log(score);
+
+const balance = new Number(400);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 23.5698;
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+// +++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-2));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.max(4, 2, 8, 6, 9));
+// console.log(Math.min(4, 2, 8, 6, 9));
+
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + 1);
