62a32edb8b174ca386cca5ea0757074c8948fa47
zakariyabombaywala <zakariyabombaywala@gmail.com>
2024-01-30 19:20:16 +0530

    JS-IIFE

---
+++ iife.js
@@ -0,0 +1,9 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function hello() {
+  console.log("Hello users, welcome to work space");
+})();
+
+((name) => {
+  console.log(`Hello ${name}, welcome back`);
+})("zakariya");
